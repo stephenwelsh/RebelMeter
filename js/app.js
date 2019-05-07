5dd8532ce3f0701ab2e259144c2ae0fe6f26583a
Stephen Welsh <stephen.welsh@unifiedfx.com>
2019-05-07 15:24:09 +0100

    Add authentication

--- js/app.js
+++ js/app.js
@@ -1,4 +1,19 @@
-var ca = new carina.Carina().open();
+
+var urlParams = new URLSearchParams(window.location.search);
+var token = urlParams.get('token');
+var clientId = urlParams.get('clientId');
+var redirectUrl = window.location; //https://stephenwelsh.github.io/RebelMeter/
+var scope = 'user:act_as'; //user:act_as channel:details:self
+if(!token){
+    window.location = `https://mixer.com/oauth/authorize?response_type=token&redirect_uri=${redirectUrl}&scope=${scope}&client_id=${clientId}`;
+}
+console.log('Auth Token', token);
+var options = {
+    authToken: token
+};
+
+var ca = new carina.Carina(options).open();
+
 ca.subscribe('channel:1:update', function (data) {
     console.log('Channel update', data);
 });
