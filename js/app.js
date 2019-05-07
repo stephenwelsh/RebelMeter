134b254c471af1238b8a690d05cf68f4f75bfd26
Stephen Welsh <stephen.welsh@unifiedfx.com>
2019-05-07 13:33:29 +0100

    Spark events

--- js/app.js
+++ js/app.js
@@ -2,3 +2,9 @@ var ca = new carina.Carina().open();
 ca.subscribe('channel:1:update', function (data) {
     console.log('Channel update', data);
 });
+ca.subscribe('channel:1:patronageUpdate', function (data) {
+    console.log('Channel1 skill update', data);
+});
+ca.subscribe('channel:35122269:patronageUpdate', function (data) {
+    console.log('Rebel skill update', data);
+});
