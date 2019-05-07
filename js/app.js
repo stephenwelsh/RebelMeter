f75e4568d97f042e9f68a07c92939a64a80ebe16
Stephen Welsh <stephen.welsh@unifiedfx.com>
2019-05-07 16:17:40 +0100

    Update

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 
 var urlParams = new URLSearchParams(window.location.search);
 var token = urlParams.get('token');
-var clientId = urlParams.get('clientid');
 
-var redirectUrl = encodeURIComponent(window.location); //https://stephenwelsh.github.io/RebelMeter/
+//var redirectUrl = encodeURIComponent(window.location); //https://stephenwelsh.github.io/RebelMeter/
+var redirectUrl = window.location.href.split('?')[0];
+
 var scope = 'user:act_as'; //user:act_as channel:details:self
 if(!token){
+    var clientId = urlParams.get('clientid');
     var authUrl = `https://mixer.com/oauth/authorize?response_type=token&redirect_uri=${redirectUrl}&scope=${scope}&client_id=${clientId}`;
     console.log('Auth URL', authUrl);
     window.setTimeout(function(){
